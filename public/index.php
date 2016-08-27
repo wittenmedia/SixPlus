378df26b1359d49052ae1ffadd80fc4d19b24479
Dan Witten <dan@danwitten.com>
2016-08-27 13:50:53 -0400

    pseudo code in index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
 
-//include autoloader
+//@todo : include autoloader
 
 //get configuration from somewhere....
+$builder = new SixPlus\ApplicationBuilder($_SERVER, $_COOKIE, $_GET, $_FILES, $_POST);
 
 //create app instance
+$app = $builder->getApplication();
 
 //create an http server request
+$request = $builder->getServerRequest();
 
 //run the app (give request, get response)
+$response = $app->handle($request);
+
+//send response
+$response->send();

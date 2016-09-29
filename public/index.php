5da8b95a8e0c2323256bac1371c232e4d53772de
Dan Witten <dan@danwitten.com>
2016-09-28 22:47:17 -0400

    got basic http message use up and running

--- public/index.php
+++ public/index.php
@@ -3,6 +3,14 @@
 //include autoloader
 require '../vendor/autoload.php';
 
+
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals();
+
+$uri = $request->getUri();
+
+print_r($uri->getHost());
+echo "\n --- \n";
+print_r($uri->getPath());
 //get configuration from somewhere....
 //$builder = new SixPlus\ApplicationBuilder($_SERVER, $_COOKIE, $_GET, $_FILES, $_POST);
 

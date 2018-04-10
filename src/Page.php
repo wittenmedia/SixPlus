7516ee86567ab58ed1071b1c9ef5d19a92e6a25e
Dan Witten <dan@danwitten.com>
2018-04-09 23:42:15 -0400

    adding basic page classes

---
+++ src/Page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ */
+
+namespace Sixplus;
+
+class Page
+{
+
+}
---
+++ src/PageService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ */
+
+namespace SixPlus;
+
+class PageService
+{
+	public function findPage($request)
+	{
+		return new Page();
+	}
+}
